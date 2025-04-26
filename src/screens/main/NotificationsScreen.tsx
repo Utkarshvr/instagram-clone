63c4e73da8225f637d2065828713b1b3b6147a92
Utkarsh Verma <108287933+Utkarshvr@users.noreply.github.com>
2025-04-26 22:05:26 +0530

    Add no notifications message to NotificationsScreen: display a centered message when there are no notifications, improving user experience and clarity.

--- src/screens/main/NotificationsScreen.tsx
+++ src/screens/main/NotificationsScreen.tsx
@@ -1,7 +1,7 @@
 import NotificationCard from "@/components/core/card/NotificationCard";
 import FollowRequests from "@/components/core/FollowRequests";
 import { useNotifications } from "@/hooks/useNotifications";
-import { RefreshControl, ScrollView, View } from "react-native";
+import { RefreshControl, ScrollView, View, Text } from "react-native";
 import LoadingScreen from "../common/LoadingScreen";
 
 type Props = {};
@@ -19,6 +19,12 @@ const NotificationsScreen = (props: Props) => {
       }
     >
       <FollowRequests />
+
+      {notifications.length === 0 && (
+        <View className="flex-1 justify-center items-center">
+          <Text className="text-neutral-100 text-lg">No notifications</Text>
+        </View>
+      )}
 
       {notifications.map((notification) => (
         <View key={notification.id} className="mb-1">
